03ed4a24986c3eaf9f28034afe3788fc64a3628e
=yisun <=>
2018-07-12 16:46:05 +0800

    +完成股票数据请求

--- scripts/data_crawler.py
+++ scripts/data_crawler.py
@@ -11,15 +11,17 @@ from utils import question_get, request_data
 import datetime
 import json
 import codecs
+import sys
+sys.path.append('../')
 def get_result(question):
     ques = question
     no_res = []
     res = []
     step = 0
     for i, q in enumerate(ques):
-        step += 1
-        if step % 200:
+        if step % 200 == 0:
             print('It is request for the question: {0}'.format(q))
+        step += 1
         try:
             data = request_data.get_stock_info(q)
             t = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -35,7 +37,7 @@ def get_result(question):
         res.append(_res)
     return no_res, res
 if __name__ == '__main__':
-    no_res = question_get.get_question()[0:10]
+    no_res = question_get.get_question()
     data = []
     for i in range(20):
         no_res, _data = get_result(no_res)
@@ -44,7 +46,7 @@ if __name__ == '__main__':
             break
     result = {'data': data}
     file = json.dumps(result)
-    with codecs.open('./result/data.txt', 'w', 'utf8') as f:
+    with codecs.open('../result/data.txt', 'w', 'utf8') as f:
         f.write(file)
-    with codecs.open('./result/no_result.txt', 'utf8') as f:
+    with codecs.open('../result/no_result.txt', 'utf8') as f:
         f.write('/n'.join(no_res))
